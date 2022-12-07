08a063805ae922de44bc260f69e6d43e12b77ec4
miguel salazar <migueljoel80@gmail.com>
2022-12-07 15:51:24 -0500

    pagina-anime

---
+++ java.js
@@ -0,0 +1,15 @@
+// document.querySelector('.menu-btn').addEventListener('click', () =>{
+//     document.querySelector('.nav-menu').classList.toggle('show');
+// })
+
+
+// const menu = document.querySelector(".menu.btn");
+// const nav = document.querySelector(nav-menu show)
+
+
+
+
+// ScrollReveal().reveal('.showcase');scrollReveal().reveal('.news-cards',{delay: 500});
+// scrollReveal().reveal('.cards-banner-one',{delay: 500});
+// scrollReveal().reveal('.cards-banner-two',{delay: 500});
+
